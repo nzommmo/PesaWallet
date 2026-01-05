4b417054d3f86ad967a86dd6d6bc0ee30461392b
nzommmo <ericnzomo17@gmail.com>
2026-01-05 16:57:35 +0300

    Added CreateEnvelope.tsx

---
+++ app/(tabs)/CreateEnvelope.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const CreateEnvelope = () => {
+  return (
+    <View>
+      <Text>CreateEnvelope</Text>
+    </View>
+  )
+}
+
+export default CreateEnvelope
